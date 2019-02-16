8d65b25716d2f690ac3633361ee34a5b267e8186
Georgy Gigauri <gkgio@users.noreply.github.com>
2019-02-16 20:43:47 +0300

    Update ErrorDTO.kt
    refactoring error handler file

--- app/src/main/java/com/gkgio/translate/helpers/errorHandling/ErrorDTO.kt
+++ app/src/main/java/com/gkgio/translate/helpers/errorHandling/ErrorDTO.kt
@@ -2,9 +2,6 @@ package com.gkgio.translate.helpers.errorHandling
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * {"code":6,"message":"Path not found"}
- */
 class ErrorDTO {
   @SerializedName("code")
   var code: Int = 0
